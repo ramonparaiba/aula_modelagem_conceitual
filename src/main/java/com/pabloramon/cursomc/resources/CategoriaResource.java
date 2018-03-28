4202414c1b6088718c6f2f899264ec3bb377098a
ramonparaiba <ramonparaiba@gmail.com>
2018-03-27 23:56:10 -0300

    Criaçao d Categoria Services

--- src/main/java/com/pabloramon/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/pabloramon/cursomc/resources/CategoriaResource.java
@@ -3,24 +3,34 @@ package com.pabloramon.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pabloramon.cursomc.domain.Categoria;
+import com.pabloramon.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping (method=RequestMethod.GET)
-	public	List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+	@RequestMapping (value="/{id}", method=RequestMethod.GET)
+	public	ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+//		Categoria cat1 = new Categoria(1, "Informática");
+//		Categoria cat2 = new Categoria(2, "Escritório");
+//		
+//		List<Categoria> lista = new ArrayList<>();
+//		lista.add(cat1);
+//		lista.add(cat2);
+		return ResponseEntity.ok().body(obj);
 	}
 }
+
+//A annotation @PathVariable p/que o Id da URL 'value=/{id}' vá para o ID da variável 
---
+++ src/main/java/com/pabloramon/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.pabloramon.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pabloramon.cursomc.domain.Categoria;
+import com.pabloramon.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	//Para buscar por id quando usar o Spring 2.X.X
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
